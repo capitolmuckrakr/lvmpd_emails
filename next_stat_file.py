96e59261b64a88069e8ff40ddfe9e4298ad523d3
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-16 11:40:38 -0800

    fix glob pattern; change var name

--- next_stat_file.py
+++ next_stat_file.py
@@ -3,13 +3,13 @@ from __future__ import print_function
 from glob import glob
 import sys, os
 
-def next_file(input):
+def next_file(inputfile):
     data_dir = '/Users/acohen/data/LVMPD/COM_CENTER_STATS/'
     os.chdir(data_dir)
-    last_id = get_ipython().getoutput(u"tail -1 $input | cut -d '|' -f 1 | cut -d '_' -f 1")
+    last_id = get_ipython().getoutput(u"tail -1 $inputfile | cut -d '|' -f 1 | cut -d '_' -f 1")
     next_id=int(last_id[0]) + 1
     try:
-        next_file=data_dir + glob('*'+str(next_id)+'*')[0]
+        next_file=data_dir + glob('*'+str(next_id)+'_rc*')[0]
     except:
         next_file=''
     return next_file
