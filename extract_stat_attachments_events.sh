042be0494d97fbba405a4b29ed1909fbac9b9a23
capitolmuckrakr <alex@capitolmuckraker.com>
2017-11-22 00:42:55 +0000

    set script path rel to $HOME

--- extract_stat_attachments_events.sh
+++ extract_stat_attachments_events.sh
@@ -3,5 +3,5 @@ cd $DIR_PATH
 echo "filename|type|description|event_no|disp|time|address"
 for file in *.TXT
     do
-    /Users/acohen/scripts/lvmpd_emails/extract_stat_single_attachment_events.sh $file | grep -v filename
+    ${HOME}/scripts/lvmpd_emails/extract_stat_single_attachment_events.sh $file | grep -v filename
 done
