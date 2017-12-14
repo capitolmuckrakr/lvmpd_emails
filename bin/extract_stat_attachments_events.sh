0eb38b819edffed6084e1c6b1884d89d81c2f2cf
capitolmuckrakr <alex@capitolmuckraker.com>
2017-12-14 12:17:43 -0800

    fix path for calling extract_stat_single_attachment_events

--- bin/extract_stat_attachments_events.sh
+++ bin/extract_stat_attachments_events.sh
@@ -3,5 +3,5 @@ cd $DIR_PATH
 echo "filename|type|description|event_no|disp|time|address"
 for file in *.TXT
     do
-    ${HOME}/scripts/lvmpd_emails/extract_stat_single_attachment_events.sh $file | grep -v filename
+    ${HOME}/scripts/lvmpd_emails/bin/extract_stat_single_attachment_events.sh $file | grep -v filename
 done
