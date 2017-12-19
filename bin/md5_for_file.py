a401195e6eb5cec3194711dae36c557d56e9f240
capitolmuckrakr <alex@capitolmuckraker.com>
2017-12-19 11:48:45 -0800

    fix var for filepath

--- bin/md5_for_file.py
+++ bin/md5_for_file.py
@@ -2,7 +2,7 @@ import hashlib
 
 def md5_for_file(filepath, block_size=2**20):
     md5 = hashlib.md5()
-    with open(filename , "rb" ) as f:
+    with open(filepath , "rb" ) as f:
         while True:
             data = f.read(block_size)
             if not data:
