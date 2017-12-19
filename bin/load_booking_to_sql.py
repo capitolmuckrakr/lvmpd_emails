b5d70773d46e9b0082583c60331dc15cf566d0d4
capitolmuckrakr <alex@capitolmuckraker.com>
2017-12-19 10:56:45 -0800

    change logging for booking load to catch non-sql errors

--- bin/load_booking_to_sql.py
+++ bin/load_booking_to_sql.py
@@ -11,15 +11,16 @@ db_log_file_name = log_dir + 'bookings_errs.log'
 logging.basicConfig(filename=db_log_file_name, level=logging.WARN, format='%(asctime)s %(message)s')
 logging.getLogger('sqlalchemy.engine').setLevel(logging.WARN)
 
-booking_file = sys.argv[1]
 conn = "postgresql://acohen:" + os.environ['PGPASSWORD'] + "@" + os.environ['ENDPOINT'] + ":5432/lvmetro"
-engine = create_engine(conn, echo=False)
-booking = extract_booking(booking_file)
+
 try:
+    booking_file = sys.argv[1]
+    booking = extract_booking(booking_file)
+    engine = create_engine(conn, echo=False)
     booking.to_sql('bookings',engine, if_exists='append',index_label='row_id')
 except IntegrityError:
     print('Error loading',booking_file)
     logging.error('IntegrityError loading %s',booking_file)
 except Exception as e:
     print('Error loading',booking_file,e)
-    logging.error('Error loading %s',booking_file)
+    logging.error('Error loading %s',booking_file + ', ' + str(e))
