1da6ff5b3077baa417b90ee90c62beaf2431cfe5
capitolmuckrakr <alex@capitolmuckraker.com>
2017-12-19 12:45:45 -0800

    no chunking if file size lt x; return hexdigest instead of digest

--- bin/md5_for_file.py
+++ bin/md5_for_file.py
@@ -1,11 +1,14 @@
-import hashlib
+import hashlib, os
 
 def md5_for_file(filepath, block_size=2**20):
     md5 = hashlib.md5()
     with open(filepath , "rb" ) as f:
         while True:
-            data = f.read(block_size)
+            if os.path.getsize(filepath) < block_size:
+                data = f.read()
+            else:
+                data = f.read(block_size)
             if not data:
                 break
             md5.update(data)
-        return md5.digest()
+        return md5.hexdigest()
