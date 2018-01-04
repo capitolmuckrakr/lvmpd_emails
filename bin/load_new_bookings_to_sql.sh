22e65bf2ad27cc46248297a7dd3f4eb7e4261686
Alexander Cohen <alex@capitolmuckraker.com>
2018-01-04 21:57:36 +0000

    tweaking bookings loader for new year

--- bin/load_new_bookings_to_sql.sh
+++ bin/load_new_bookings_to_sql.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+#may have to manually load first booking for new calendar year
 export SQL_DIR="${HOME}/scripts/lvmpd_emails/bin/sql/"
+export yr=$(date +%y)
 export last_booking=$(psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -f ${SQL_DIR}latest_loaded_booking.sql | head -n3 | tail -n1 | cut -d ',' -f 1)
-export bookings_count=$(ls ~/data/LVMPD/BOOKINGS/*ITAG*ALL*.htm | wc -l)
-export bookings_loaded_count=$(ls ~/data/LVMPD/BOOKINGS/*ITAG*ALL*.htm | grep -n $last_booking | cut -d ':' -f 1)
+export bookings_count=$(ls ~/data/LVMPD/BOOKINGS/*$yr-ITAG-ALL*.htm | wc -l)
+export bookings_loaded_count=$(ls ~/data/LVMPD/BOOKINGS/*$yr-ITAG-ALL*.htm | grep -n $last_booking | cut -d ':' -f 1)
 export bookings_unloaded_count=$(python -c 'import os; print int(os.environ["bookings_count"])-int(os.environ["bookings_loaded_count"])')
 OLDIFS=$IFS
 IFS=$'\n'
-for booking in $(ls ~/data/LVMPD/BOOKINGS/*ITAG*ALL*.htm | tail -n $bookings_unloaded_count)
+for booking in $(ls ~/data/LVMPD/BOOKINGS/*$yr-ITAG-ALL*.htm | tail -n $bookings_unloaded_count)
 do
 echo 'loading' $booking
 ipython load_booking_to_sql.py $booking
