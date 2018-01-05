d28056729cfb58a4d1b9d126a01646626241503d
Alexander Cohen <alex@capitolmuckraker.com>
2018-01-05 22:04:50 +0000

    add eventdate in sql format as col

--- bin/extract_stat_attachments_date_time.sh
+++ bin/extract_stat_attachments_date_time.sh
@@ -1,11 +1,14 @@
 DIR_PATH=$1
 cd $DIR_PATH
-echo "filename|filedate|filetime|starttime|endtime"
+echo "filename|filedate|filetime|eventdate|starttime|endtime"
 for file in *.TXT
     do
     filedate=$(head -1 $file | cut -d ':' -f 3 | sed -e 's/^[ ]*//g' | sed -e 's/ /\|/g' | cut -d '|' -f 1)
     filetime=$(head -1 $file | cut -d ':' -f 4,5 | sed -e 's/^[ ]*//g' | sed -e 's/ /\|/g' | cut -d '|' -f 1)
-    starttime=$(head -3 $file | tail -1 | sed -e 's/^[ ]*//g' | awk '{gsub(/ +/," ")} {print $0}' | sed -e 's/ /\|/g' | cut -d '|' -f 5)
-    endtime=$(head -3 $file | tail -1 | sed -e 's/^[ ]*//g' | awk '{gsub(/ +/," ")} {print $0}' | sed -e 's/ /\|/g' | cut -d '|' -f 7)
-    echo $file"|"$filedate"|"$filetime"|"$starttime"|"$endtime
+    eventdatetime=$(head -3 $file | tail -1 | sed -e 's/^[ ]*//g' | awk '{gsub(/ +/," ")} {print $0}')
+    export eventdate=$(echo $eventdatetime | sed -e 's/ /\|/g' | cut -d '|' -f 2,3,4 | sed -e 's/|/ /g')
+    export eventdate=$(python -c 'import datetime, os; string_date = os.environ["eventdate"]; fmt = "%B %d, %Y"; print(datetime.datetime.strptime(string_date, fmt).strftime("%m/%d/%Y"))')
+    starttime=$(echo $eventdatetime | sed -e 's/ /\|/g' | cut -d '|' -f 5)
+    endtime=$(echo $eventdatetime | sed -e 's/ /\|/g' | cut -d '|' -f 7)
+    echo $filename"|"$filedate"|"$filetime"|"$eventdate"|"$starttime"|"$endtime
 done
