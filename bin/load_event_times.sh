ed69329686d309d0f7cfb4b6e88db71cabe28378
capitolmuckrakr <alex@capitolmuckraker.com>
2018-01-05 16:17:50 -0800

    add evendate col to loader shell script

--- bin/load_event_times.sh
+++ bin/load_event_times.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CSV_PATH=$1
-cat $CSV_PATH | psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -c "\copy event_times (filename,filedate,filetime,starttime,endtime) FROM '$CSV_PATH' WITH DELIMITER '|' NULL '' CSV HEADER "
+cat $CSV_PATH | psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -c "\copy event_times (filename,filedate,filetime,eventdate,starttime,endtime) FROM '$CSV_PATH' WITH DELIMITER '|' NULL '' CSV HEADER "
