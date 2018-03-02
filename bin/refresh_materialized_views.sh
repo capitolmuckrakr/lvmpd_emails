0a731e89769784caee05a6ba14a3094552d2e0a6
capitolmuckrakr <alex@capitolmuckraker.com>
2018-03-02 15:05:39 -0800

    add child bookings to refreshed views

--- bin/refresh_materialized_views.sh
+++ bin/refresh_materialized_views.sh
@@ -2,3 +2,4 @@
 export SQL_DIR="${HOME}/scripts/lvmpd_emails/bin/sql/"
 psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -f ${SQL_DIR}refresh_bookings_view.sql
 psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -f ${SQL_DIR}refresh_calls_view.sql
+psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -f ${SQL_DIR}refresh_child_related_bookings_view.sql
