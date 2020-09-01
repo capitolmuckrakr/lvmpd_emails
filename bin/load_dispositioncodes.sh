ff0d0752819ed03a61954dac4a405c7f85d41a1a
Alexander Cohen @capitolmuckrakr <alex@capitolmuckraker.com>
2020-09-01 09:58:41 -0400

    fix typo duplication of file var

--- bin/load_dispositioncodes.sh
+++ bin/load_dispositioncodes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATAPATH="${HOME}/data/LVMPD/police_codes.txt"
-cat $DATAPATHDATAPATH | psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -c "\copy lk_dispositioncodes (code,definition) FROM '$DATAPATH' "
+cat $DATAPATH | psql --host=${ENDPOINT} --port=5432 --username=acohen --dbname=lvmetro -c "\copy lk_dispositioncodes (code,definition) FROM '$DATAPATH' "
